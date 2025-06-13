2df1b4ae20e76dca20dfa2d884446a15ed8a6af2
Joe Vimar <trungvimar@gmail.com>
2025-06-13 01:36:25 -0700

    change qr file name

--- laravel/app/Jobs/GenerateUserQrCode.php
+++ laravel/app/Jobs/GenerateUserQrCode.php
@@ -33,7 +33,13 @@ class GenerateUserQrCode implements ShouldQueue
     public function handle()
     {
         $encodedAddress = urlencode($this->address);
-        $fileName = $this->userid . '_' . Str::random(6) . '.png';
+        
+        // Get the user's first name and current timestamp
+        $user = \App\Models\User::find($this->userid);
+        $firstName = explode(' ', $user->name)[0];  // Get first name
+        $timestamp = now()->format('Y-m-d_H-i-s');  // Format: YYYY-MM-DD_HH-mm-ss
+        $fileName = $firstName . '_' . $timestamp . '.png';
+        
         $url = 'https://api.qrserver.com/v1/create-qr-code/?data=' . $encodedAddress . '&size=150x150';
         // // remember to copy cacert.pem to server to make this work
         $qrresponse = Http::get($url);
